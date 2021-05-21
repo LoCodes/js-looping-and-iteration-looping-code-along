3743ce6017228b486561eb964843b2566be71dbf
Loren Cipriano <loren25cipriano@gmail.com>
2021-05-20 20:41:19 -0700

    commit tree c4ba1f34959652bb3f6e1bdaebbdf28f1ffb82ca

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+function writeCards(names, event ){
+    let cards = []
+    for ( let i = 0; i < names.length; i ++ ) {
+        let message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+        console.log(message)  //Printing Message 
+        cards.push(message)   //Putting Message in box
+    }
+    return cards; //Return array with message.
+    
+}
+
+
+function countDown(number) {
+    while (number > 0 ){
+        console.log(number--);
+
+    }
+    console.log(number)
+}
